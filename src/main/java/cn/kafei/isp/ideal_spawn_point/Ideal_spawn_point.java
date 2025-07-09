898e9e82d07da9a35ec641545b206d05d4c9bc47
kafei520-CN <1780412693@qq.com>
2025-07-09 13:44:36 +0800

    创建仓库

---
+++ src/main/java/cn/kafei/isp/ideal_spawn_point/Ideal_spawn_point.java
@@ -0,0 +1,17 @@
+package cn.kafei.isp.ideal_spawn_point;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Ideal_spawn_point extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
